6a45b48cd588f48edbe18a0a1ce7093d00378797
RileyYoung <youn0628@algonquinlive.com>
2022-11-02 09:17:41 -0400

    Initial commit; near empty uproject with UMG and Paper2d included.

---
+++ Source/Breakout/Ball.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Ball.h"
+#include "PaperSpriteComponent.h"
+
+// Sets default values
+ABall::ABall() : elapsedTime(0.0f)
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ABall::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABall::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
